0813fee38fb60c07c64f2fe0bfc8219a6c7e83b1
Zampagnone <veroneslorenzo@gmail.com>
2024-09-13 11:18:08 +0200

    quality of life improvements

--- qrCode.py
+++ qrCode.py
@@ -4,8 +4,7 @@ from tkinter import ttk
 
 win= Tk()
 
-#Set the geometry of Tkinter frame
-win.geometry("750x250")
+win.geometry("500x150")
 
 def createQr():
     global entry
@@ -22,19 +21,19 @@ def createQr():
     img = qr.make_image()
     with open('qrcode.png', 'wb') as f:
             img. save(f)
+    label1.configure(text="qr-code saved, check the folder", fg="green")
     
-
-#Initialize a Label to display the User Input
-label=Label(win, text="", font=("Courier 22 bold"))
+label=Label(win, text="", font=("Modern 22 bold"))
 label.pack()
 label.configure(text="Insert the desired url")
 
-#Create an Entry widget to accept User Input
 entry= Entry(win, width= 40)
 entry.focus_set()
 entry.pack()
 
-#Create a Button to validate Entry Widget
 ttk.Button(win, text= "Ok",width= 20, command= createQr).pack(pady=20)
+
+label1=Label(win, text="", font=("Modern 12 bold"))
+label1.pack()
 
 win.mainloop()
